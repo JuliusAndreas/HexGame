c504cb7dd5105949a4d753376d652da14081d8b4
Soroosh Faal <faalsoroosh@gmail.com>
2024-05-18 20:07:01 +0330

    add more activities

--- app/src/main/java/com/julius/hexgame/PlayerConfigActivity.java
+++ app/src/main/java/com/julius/hexgame/PlayerConfigActivity.java
@@ -3,6 +3,7 @@ package com.julius.hexgame;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,22 +11,27 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class PlayerConfigActivity extends AppCompatActivity {
+    EditText edtTextPlayerOneName;
+    EditText edtTextPlayerTwoName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_player_config);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        edtTextPlayerOneName = findViewById(R.id.playerOneName);
+        edtTextPlayerTwoName = findViewById(R.id.playerTwoName);
     }
-
-    public void goToPVPConfig(View view) {
-        Intent intent = new Intent(this, PlayerConfigActivity.class);
+    public void goToGamePlayAcitivity(View view){
+        Intent intent = new Intent(this, GamePlayActivity.class);
+        intent.putExtra("player_one_name", edtTextPlayerOneName.getText().toString());
+        intent.putExtra("player_two_name", edtTextPlayerTwoName.getText().toString());
         startActivity(intent);
     }
 }
